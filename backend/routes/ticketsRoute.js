248df50d6989c8b38a62892f2213b08c1f5ef847
Karel Šťastný <karell29@icloud.com>
2023-10-10 16:34:30 +0200

    Created function for reading ticket in databse, and thier count.

--- backend/routes/ticketsRoute.js
+++ backend/routes/ticketsRoute.js
@@ -1,5 +1,5 @@
 import express, { request, response } from "express";
-import { Ticket } from "../models/ticketModel.js"
+import { Ticket } from "../models/ticketModel.js";
 
 const router = express.Router();
 
@@ -7,40 +7,52 @@ const router = express.Router();
 router.post("/", async (request, response) => {
   try {
     // Kontrola zda jsou vyplěná poviná pole
-    if (
-      !request.body.band ||
-      !request.body.price ||
-      !request.body.account
-    ) {
-        // Pokud nejsou všechna vyplněná, vraď chybovou odpověd
-      return response
-        .status(400)
-        .send({
-          message:
-            "You have to fill in all requred input: band, price, account",
-        });
+    if (!request.body.band || !request.body.price || !request.body.account) {
+      // Pokud nejsou všechna vyplněná, vraď chybovou odpověd
+      return response.status(400).send({
+        message: "You have to fill in all requred input: band, price, account",
+      });
     }
     // Vytvoření nového objektu na základě všech přijatých dat
     const newTicket = {
-        band: request.body.band,
-        price: request.body.price,
-        exchangeStock: request.body.exchangeStock,
-        country: request.body.country,
-        datePurkrase: request.body.datePurkrase,
-        account: request.body.account,
-        typeOfTicket: request.body.typeOfTicket,
-        order: request.body.order,
-    }
+      band: request.body.band,
+      price: request.body.price,
+      exchangeStock: request.body.exchangeStock,
+      country: request.body.country,
+      datePurkrase: request.body.datePurkrase,
+      account: request.body.account,
+      typeOfTicket: request.body.typeOfTicket,
+      order: request.body.order,
+    };
 
     // Vytvoření lístku ze schématu
-    const ticket = await Ticket.create(newTicket)
+    const ticket = await Ticket.create(newTicket);
 
     // odeslání do databáze
-    return response.status(201).send(ticket)
-
+    return response.status(201).send(ticket);
   } catch (error) {
     console.log(error.message);
-    response.status(500).send({message:error.message})
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
+
+// Požadavek na přečtení vypsání lístků
+router.get("/", async (request, response) => {
+  try {
+
+    // Najdi vše co najdeš uložené v lístkách
+    const tickets = await Ticket.find({});
+
+    // Ulož odpověď do dat a přidje k nim ještě součet všech zánamů
+    return response.status(200).json({
+      conut: tickets.length,
+      data: tickets,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
   }
 });
 
